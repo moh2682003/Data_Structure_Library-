b1210e486e169ef7dc72329a6d27b4bd9b0f5c6e
Your Name <you@email.com>
2026-03-07 01:51:07 +0200

    first commit

---
+++ UtilityLibrary.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include "clsUtility.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	 cout << clsUtility::Round(3.2) << endl ;
+	 cout << clsUtility::Round(-3.2) << endl ;
+	 cout << clsUtility::Round(3.7) << endl;
+
+}
